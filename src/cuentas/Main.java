ac82b59034bed77841530978401f780ec2f32929
Omar De Miguel <>
2023-02-12 18:47:46 +0100

    Segundo commit

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -1,3 +1,5 @@
+package cuentas;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -11,21 +13,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        saldoActual = miCuenta.estado();
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es" + saldoActual);
 
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
