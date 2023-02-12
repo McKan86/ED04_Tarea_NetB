11b38eba0c320cd4defc452a92561ed70c9c3d9a
Omar De Miguel <>
2023-02-12 21:29:58 +0100

    Tercer commit

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,25 +1,65 @@
+/**
+ *
+ * Clase que representa una cuenta bancaria
+ *
+ * @author Omar De Miguel
+ */
 package cuentas;
 
 public class CCuenta {
 
+    /**
+     * Nombre del titular de la cuenta
+     */
     private String nombre;
+    /**
+     * Número de cuenta
+     */
     private String cuenta;
+    /**
+     * Saldo actual de la cuenta
+     */
     private double saldo;
+    /**
+     * Tipo de interés asociado a la cuenta
+     */
     private double tipoInterés;
 
+    /**
+     * Crea una nueva instancia de la clase CCuenta con valores por defecto
+     */
     public CCuenta() {
     }
 
+    /**
+     * Constructor que inicializa todos los atributos de la cuenta.
+     *
+     * @param nom Nombre del titular de la cuenta
+     * @param cue Número de cuenta
+     * @param sal Saldo actual de la cuenta
+     * @param tipo Tipo de interés asociado a la cuenta
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
         saldo = sal;
     }
 
+    /**
+     * Método que devuelve el saldo actual de la cuenta.
+     *
+     * @return Saldo actual en la cuenta.
+     */
     public double estado() {
         return getSaldo();
     }
 
+    /**
+     * Método que permite ingresar una cantidad en la cuenta.
+     *
+     * @param cantidad Cantidad a ingresar en la cuenta.
+     * @throws Exception Si la cantidad es negativa.
+     */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
@@ -27,6 +67,12 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Método que permite retirar una cantidad de la cuenta.
+     *
+     * @param cantidad Cantidad a retirar de la cuenta.
+     * @throws Exception Si la cantidad es negativa o el saldo es insuficiente.
+     */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0) {
             throw new Exception("No se puede retirar una cantidad negativa");
@@ -38,56 +84,72 @@ public class CCuenta {
     }
 
     /**
-     * @return the nombre
+     * Devuelve el nombre del titular de la cuenta.
+     *
+     * @return Nombre del titular de la cuenta.
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Establece el nombre del titular de la cuenta
+     *
+     * @param nombre El nombre a establecer
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * Método que devuelve el número de cuenta
+     *
+     * @return El número de cuenta como una cadena de texto
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Método que permite establecer el número de cuenta
+     *
+     * @param cuenta El nuevo número de cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Método que devuelve el saldo actual de la cuenta
+     *
+     * @return El saldo actual de la cuenta
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Método que permite establecer el saldo de la cuenta
+     *
+     * @param saldo El nuevo saldo de la cuenta
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInterés
+     * Método que devuelve el tipo de interés actual de la cuenta
+     *
+     * @return El tipo de interés actual de la cuenta
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
-     * @param tipoInterés the tipoInterés to set
+     * Método que permite establecer el tipo de interés de la cuenta
+     *
+     * @param tipoInterés El nuevo tipo de interés de la cuenta
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
